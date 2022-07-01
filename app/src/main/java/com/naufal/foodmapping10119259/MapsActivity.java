53e344c7f254756e6dae083cc2bd3e2c1da3ff72
Nopss <65601217+Nopss27@users.noreply.github.com>
2022-07-01 21:55:33 +0700

    Update MapsActivity.java

--- app/src/main/java/com/naufal/foodmapping10119259/MapsActivity.java
+++ app/src/main/java/com/naufal/foodmapping10119259/MapsActivity.java
@@ -1,5 +1,10 @@
 package com.naufal.foodmapping10119259;
 
+/*
+ * Naufal Daffa Ilyasa
+ * 10119259
+ * IF7*/
+
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.annotation.RequiresPermission;
